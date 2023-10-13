2c3dabccff1ecceb3d3101f79cdf2307605a40d6
sampreetucalgary07 <sampreet.vaidya@ucalgary.ca>
2023-10-13 15:33:39 -0600

    updates till oct 13

--- batchTraining_Model.py
+++ batchTraining_Model.py
@@ -1,5 +1,5 @@
 
-from subImage import get_sub_image, patch_sizes, get_tensor, append_posAndImg, append_pos
+from subImage import get_sub_image, patch_sizes, get_tensor, append_posAndImg, append_pos, create_difference_heatmap
 from tqdm import tqdm
 
 
@@ -87,6 +87,58 @@ def training_model_pixelSingle(trainR0, trainR1, model, patch_list,
                 loss.backward()
                 opt.step()
         print(f"loss : {loss}")
+        loss_list.append(loss)
+        epoch_list.append(epoch)
+
+    return epoch_list, loss_list
+
+
+def training_model_pixelCondition(trainR0, trainR1, model, patch_list,
+                                  applyFunc, applyFuncR0,
+                                  criterion, epochs, opt,
+                                  ):
+    epoch_list = []
+    loss_list = []
+
+    sub_image_size = int(patch_list[1] - patch_list[0])
+    for epoch in range(epochs):
+        print(f"Epoch : {epoch+1}")
+        model.train()
+        diff_list = []
+        for i, (r0, r1) in tqdm(enumerate(zip(trainR0, trainR1)), desc="Training Samples : ", total=len(trainR0)):
+
+            diff, _ = create_difference_heatmap(
+                r1[0], r0[0], "Difference Heatmap", show=False)
+            if diff < 4:
+                continue
+            diff_list.append(diff)
+            r0_sub = get_sub_image(r0, patch_list)
+            # l0_sub = get_sub_image(l0, patch_list)
+            r1_sub = get_sub_image(r1, patch_list)
+            sub_patch_list = patch_sizes(sub_image_size, 1)
+            min_value = 1
+            max_value = len(list(sub_patch_list.values()))
+
+            for enum, sub_patch_list in enumerate(list(sub_patch_list.values())):
+                # print(enum)
+                # print(sub_patch_list)
+                p_no = ((enum+1) - min_value) / (max_value - min_value)
+                opt.zero_grad()
+                r0_64_sub = get_sub_image(
+                    r0_sub, sub_patch_list, data_loader_inst=False)
+                r1_64_sub = get_sub_image(
+                    r1_sub, sub_patch_list, data_loader_inst=False)
+
+                r0_64_sub = applyFuncR0(subTensor=r0_64_sub)
+                r1_64_sub = applyFunc(
+                    subTensor=r1_64_sub, patch_no=p_no, img_type=1.0)
+
+                outputs = model(r1_64_sub)
+                loss = criterion(outputs, r0_64_sub)
+                loss.backward()
+                opt.step()
+        print(f"loss : {loss}")
+        print(f"Diff : {diff_list}")
         loss_list.append(loss)
         epoch_list.append(epoch)
 
